5f347375cee2f507e9499b2a5272175084fe9af6
bszaj <b.szajor@gmail.com>
2021-05-08 22:22:29 +0200

    dodane zdj

--- app/src/main/java/com/example/yummy/GalleryActivity.java
+++ app/src/main/java/com/example/yummy/GalleryActivity.java
@@ -2,13 +2,55 @@ package com.example.yummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.Serializable;
 
 public class GalleryActivity extends AppCompatActivity {
+    ImageView image1, image2, image3, image4,image5,image6, image7, image8, image9,image10,image11,image12,
+            image13, image14,image15,image16, image17,image18,image19,image20;
+    TextView text1,text2,text3,text4,text5
+            ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        image1 = findViewById(R.id.image1);
+        image2 = findViewById(R.id.image2);
+        image3 = findViewById(R.id.image3);
+        image4 = findViewById(R.id.image4);
+        image5 = findViewById(R.id.image5);
+        image6 = findViewById(R.id.image6);
+        image7 = findViewById(R.id.image7);
+        image8 = findViewById(R.id.image8);
+        image9 = findViewById(R.id.image9);
+        image10 = findViewById(R.id.image10);
+        image11 = findViewById(R.id.image11);
+        image12 = findViewById(R.id.image12);
+        image13 = findViewById(R.id.image13);
+        image14 = findViewById(R.id.image14);
+        image15 = findViewById(R.id.image15);
+        image16 = findViewById(R.id.image16);
+        image17 = findViewById(R.id.image17);
+        image18 = findViewById(R.id.image18);
+        image19 = findViewById(R.id.image19);
+        image20 = findViewById(R.id.image20);
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text22);
+        text3 = findViewById(R.id.text3);
+        text4 = findViewById(R.id.text4);
+        text5 = findViewById(R.id.text5);
+
+    }
+
+    public void onClick(View view) {
+
     }
 }
