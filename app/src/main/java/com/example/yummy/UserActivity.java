7700f0de840d2eddb6518bc8b964577f4642b801
jordan819 <patryk.zaucha00@gmail.com>
2021-06-13 00:05:25 +0200

    added display, cleaned code

--- app/src/main/java/com/example/yummy/UserActivity.java
+++ app/src/main/java/com/example/yummy/UserActivity.java
@@ -3,12 +3,23 @@ package com.example.yummy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.yummy.service.LoggedUser;
 
 public class UserActivity extends AppCompatActivity {
+
+    TextView nick, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        nick = findViewById(R.id.user_nick);
+        email = findViewById(R.id.user_mail);
+
+        nick.setText(LoggedUser.getNick());
+        email.setText(LoggedUser.getEmail());
     }
 }
